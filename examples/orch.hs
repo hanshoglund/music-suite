6d269f74c467f8092478d3b425ffb2f394d120d9
Hans Hoeglund <hans@hanshoglund.se>
2020-05-24 11:11:25 +0100

    Piece

--- examples/orch.hs
+++ examples/orch.hs
@@ -15,13 +15,27 @@ import qualified Ex.Voicing
 
 main = defaultMain music
 
+-- TODO make most/all of these into functions
+-- Explore variations of *similar* material
+-- Edit/manipulate parameters to turn into some kind of logical sequence
+--
+-- TODO transcribe more from manual notes. Try to make everything into functions
+-- (find some parameter to vary)
 music =
   times 1 $ pseq
   [ P.SustainPunctuated.Score.music
+
   , Ex.StringTexture.music
+
   , Ex.Sky.music
+
+  -- TODO contrary motion of chrods, a la Tan Dun "Map"
+
+  -- TODO make proper chords, orchestrate
   , Ex.Voicing.music
+
   , P.WindsPhrasing.Score.music
+
   , arrangeFor (divide 3 trumpets ++ divide 4 horns ++ divide 2 trombones)
     $ P.BrassLargeEnsemble.Score.music
   ]
