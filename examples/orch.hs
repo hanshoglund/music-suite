14986db5c72156a6e434ec2f998e0a557b816ccf
Hans Hoeglund <hans@hanshoglund.se>
2020-05-24 11:11:25 +0100

    Piece

--- examples/orch.hs
+++ examples/orch.hs
@@ -48,36 +48,57 @@ multiTempoCanon' entries subj = mconcat $ fmap
 -- For example Le Sacre "Dances of the Young Girls" is a "musicalization" of [Bool]
 music =
   times 1 $ pseq
-  [ P.SustainPunctuated.Score.music
+  [ mempty
 
-  , Ex.StringTexture.music
+  , multiTempoCanon'
+    [ (divide 2 cellos !! 0, _P5,        4 >-> (2/3))
+    , (divide 2 cellos !! 1, _P1,        2 >-> 1)
+    , (divide 2 doubleBasses !! 0, -_P4, 1 >-> 1.5)
+    , (divide 2 doubleBasses !! 1, -_P8, 0 >-> 2)
+    ]
+    (view voice [c,d,e|*2,c,d,d,e|*1.5,e,f,d,e,c|*2,d|*2])
+
+  -- TODO to use thirds we might want diatonic transposition
+  -- instead of real
+  , flip renderPattern (0 <-> 30) $ multiTempoCanon
+    [ (divide 2 violas !! 0, _P8+_P5,        4 >-> (5/6))
+    , (divide 2 cellos !! 0, _P5,        4 >-> (2/3))
+    , (divide 2 cellos !! 1, _P1,        2 >-> 1)
+    , (divide 2 doubleBasses !! 0, -_P4, 1 >-> 1.5)
+    , (divide 2 doubleBasses !! 1, -_P8, 0 >-> 2)
+    ]
+    (view voice [c,d,e|*2,c,d,d,e|*1.5,e,f,d,e,c|*2,d|*2])
+
+  -- , P.SustainPunctuated.Score.music
+
+  -- , Ex.StringTexture.music
 
 
-  -- TODO more patterns (a la Interludes)
+  -- -- TODO more patterns (a la Interludes)
 
-  -- TODO more floaters (a la Mist)
+  -- -- TODO more floaters (a la Mist)
 
-  -- TODO multitempo canons (a la Passages/Imitations)
+  -- -- TODO multitempo canons (a la Passages/Imitations)
 
-  -- TODO large-scale Behavior (a la Shadowings/Layers)
+  -- -- TODO large-scale Behavior (a la Shadowings/Layers)
 
-  , Ex.Sky.music
+  -- , Ex.Sky.music
 
-  -- TODO palindrome phased patterns, a la Calliano "When I Dream"
-  --  [[Pitch]] -> Pattern (Part, Pitch)
+  -- -- TODO palindrome phased patterns, a la Calliano "When I Dream"
+  -- --  [[Pitch]] -> Pattern (Part, Pitch)
 
-  -- TODO contrary motion of chords, a la Tan Dun "Map"
-  --  { origChord :: Chord Interval Pitch
-  --  , invChord  :: Chord Interval Pitch
-  --  , tune      :: Voice Pitch
-  --  } -> Pattern (Part, Pitch)
+  -- -- TODO contrary motion of chords, a la Tan Dun "Map"
+  -- --  { origChord :: Chord Interval Pitch
+  -- --  , invChord  :: Chord Interval Pitch
+  -- --  , tune      :: Voice Pitch
+  -- --  } -> Pattern (Part, Pitch)
 
-  -- TODO make proper chords, orchestrate
-  , Ex.Voicing.music
+  -- -- TODO make proper chords, orchestrate
+  -- , Ex.Voicing.music
 
-  , P.WindsPhrasing.Score.music
+  -- , P.WindsPhrasing.Score.music
 
-  , arrangeFor (divide 3 trumpets ++ divide 4 horns ++ divide 2 trombones)
-    $ P.BrassLargeEnsemble.Score.music
+  -- , arrangeFor (divide 3 trumpets ++ divide 4 horns ++ divide 2 trombones)
+  --   $ P.BrassLargeEnsemble.Score.music
   ]
 
